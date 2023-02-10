f5f33ec1cb24a1f9557a7c4224225ea5da60f408
bonefabric <iburanguloff@ya.ru>
2023-02-10 17:57:05 +0300

    config loading refactoring

--- config/yaml/load.go
+++ config/yaml/load.go
@@ -2,9 +2,7 @@ package yaml
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 )
@@ -18,26 +16,17 @@ type appConfig struct {
 	StoreDriver   string `yaml:"store-driver"`
 }
 
-func Load(path string) (conf *Config, err error) {
-	file, err := os.Open(filepath.Clean(path))
-	if err != nil {
-		return nil, fmt.Errorf("failed to load config file: %s", err)
-	}
-	defer func(file *os.File) {
-		if e := file.Close(); e != nil && err == nil {
-			err = e
-		}
-	}(file)
-
-	data, err := io.ReadAll(file)
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read config file: %s", err)
 	}
 
 	var c config
-	if err = yaml.Unmarshal(data, &c); err != nil {
+	if err := yaml.Unmarshal(data, &c); err != nil {
 		return nil, fmt.Errorf("failed to parse config file: %s", err)
 	}
+
 	return &Config{
 		tgToken:     c.App.TelegramToken,
 		storeDriver: c.App.StoreDriver,
