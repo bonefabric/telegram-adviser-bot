599d51e9bc41970eb38d98782200bd6bfc23c716
bonefabric <iburanguloff@ya.ru>
2023-02-10 18:48:12 +0300

    yaml config test

---
+++ config/yaml/yaml_test.go
@@ -0,0 +1,63 @@
+package yaml
+
+import (
+	"os"
+	"testing"
+)
+
+func TestLoadSuccess(t *testing.T) {
+	validYaml := `
+app:
+  telegram-token: "6084774046"
+  store-driver: "sqlite3"
+`
+	tmpFile, err := os.CreateTemp(".", "config.*.yaml")
+	if err != nil {
+		t.Fatalf("failed to create temp file: %s", err)
+	}
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(tmpFile.Name())
+
+	_, err = tmpFile.Write([]byte(validYaml))
+	if err != nil {
+		t.Fatalf("failed to write data to temp file: %s", err)
+	}
+
+	conf, err := Load(tmpFile.Name())
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	if conf == nil {
+		t.Error("nil config")
+	}
+	if conf.TelegramToken() != "6084774046" {
+		t.Errorf("incorrect tgToken: %s", conf.TelegramToken())
+	}
+	if conf.StoreDriver() != "sqlite3" {
+		t.Errorf("incorrect storeDriver: %s", conf.StoreDriver())
+	}
+}
+
+func TestLoadFail(t *testing.T) {
+	invalidYaml := `
+telegram-token = 6084774046
+`
+	tmpFile, err := os.CreateTemp(".", "config.*.yaml")
+	if err != nil {
+		t.Fatalf("failed to create temp file: %s", err)
+	}
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(tmpFile.Name())
+
+	_, err = tmpFile.Write([]byte(invalidYaml))
+	if err != nil {
+		t.Fatalf("failed to write data to temp file: %s", err)
+	}
+
+	_, err = Load(tmpFile.Name())
+	if err == nil {
+		t.Error("error nil on invalid file")
+	}
+}
