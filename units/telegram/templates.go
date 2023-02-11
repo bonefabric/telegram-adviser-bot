e93e0e6275399849fd9f69c3de4b9db2034a4468
bonefabric <iburanguloff@ya.ru>
2023-02-11 19:38:25 +0300

    upd helpMessage

--- units/telegram/templates.go
+++ units/telegram/templates.go
@@ -11,24 +11,6 @@ Starts a new session with the bot.
 ## /help
 Displays a list of available commands and their usage.
 
-## /weather [city name]
-Displays the current weather conditions for the specified city.
-
-## /setlocation [city name]
-Sets the default location for weather queries.
-
-## /forecast [city name]
-Displays the 5-day weather forecast for the specified city.
-
-## /settings
-Displays the current bot settings.
-
-## /feedback [message]
-Sends feedback to the bot developers.
-
-## /cancel
-Cancels the current operation and returns to the main menu.
-
 ---
 
 If you need further assistance or have any questions, feel free to contact us.
